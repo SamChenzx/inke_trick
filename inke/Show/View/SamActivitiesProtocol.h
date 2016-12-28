5200936a3d9d5856990904cf2e0f7f4682bf0fdf
SamChenzx <samchen.swift@gmail.com>
2016-12-28 10:02:54 +0800

    Add nearby collectionView header view

---
+++ inke/Show/View/SamActivitiesProtocol.h
@@ -0,0 +1,13 @@
+//
+//  SamActivitiesProtocol.h
+//  inke
+//
+//  Created by Sam on 12/28/16.
+//  Copyright © 2016 Zhejiang University of Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SamActivitiesProtocol <NSObject>
+
+@end
